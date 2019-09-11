6fddbbda700083c171ccd3677a45d747e70b2efb
niksmitty <niksmitty@rambler.ru>
2019-09-11 22:54:02 +0500

    Some changes after initial commit (first tests, documentation and other settings in project).

--- CCNEvaluator/CCNEvaluator.swift
+++ CCNEvaluator/CCNEvaluator.swift
@@ -98,12 +98,21 @@ public extension CCNEvaluator {
 // MARK: Definition the type of network to which credit card number belongs
 public extension CCNEvaluator {
     
+    /**
+     * @brief Returns network type (card type) of credit card
+                validating prefix specifications only
+     * @param creditCardNumber Credit card number
+     * @return String Credit card network type (can be recognized only
+                        amongst the cases of CardNetworkType enumeration)
+     **/
     class func getCreditCardNumberNetworkType(creditCardNumber number: String) -> String {
         let number = number.removeAllWhitespacesAndNewlines()
         
         return CardNetworkType.type(for: number)
     }
     
+    // `CardNetworkType` is the credit card network type name,
+    // used to name concrete card network type
     internal enum CardNetworkType: String, CaseIterable {
         
         case americanExpress = "American Express"
@@ -117,10 +126,20 @@ public extension CCNEvaluator {
         case visa = "Visa"
         case notRecognized = "Not recognized"
         
+        /**
+         * @brief Returns specifications for concrete network type
+         **/
         private var specifications: CCNNetworkTypeSpecifications {
             return CCNNetworkTypeSpecifications(with: self)
         }
         
+        /**
+         * @brief Checks given credit card number satisfies
+                    concrete network type specification conditions
+         * @param type Credit card network type
+         * @param creditCardNumber Credit card number
+         * @return Bool `true` if checking was passed, `false` otherwise
+         **/
         private static func validateSpecifications(for type: CardNetworkType, and creditCardNumber: String) -> Bool {
             var isValid = false
             for prefix in type.specifications.prefixes {
@@ -135,6 +154,12 @@ public extension CCNEvaluator {
             return isValid
         }
         
+        /**
+         * @brief Returns result network type which was defined
+                    by validating to specifications of different types
+         * @param creditCardNumber Credit card number
+         * @return String result network type (included not recognized case)
+         **/
         static func type(for creditCardNumber: String) -> String {
             var resultType: CardNetworkType = .notRecognized
             for type in CardNetworkType.allCases {
@@ -153,6 +178,11 @@ public extension CCNEvaluator {
 // MARK: Obtaining additional information about credit card through https://binlist.net API
 public extension CCNEvaluator {
     
+    /**
+     * @brief Retrieves additional information about credit card using https://binlist.net API
+     * @param creditCardNumber Credit card number
+     * @param completion Use this completion block for getting result information string
+     **/
     class func getCreditCardAdditionalInformation(creditCardNumber number: String, _ completion: @escaping (String) -> Void) {
         let number = number.removeAllWhitespacesAndNewlines()
         
@@ -162,6 +192,16 @@ public extension CCNEvaluator {
         }
     }
     
+    /**
+     * @brief Retrieves additional information about credit card:
+                uses APIClient instance for request performing,
+                checks status code,
+                also performs decoding to corresponding model
+     * @param creditCardNumber Credit card number
+     * @param completion Use this completion block for getting
+                            result information string or
+                            error description string
+     **/
     internal class func getInformation(creditCardNumber number: String, _ completion: @escaping (String?, String?) -> Void) {
         var informationString = Const.informationIsNotFoundString
         
--- CCNEvaluatorTests/CCNEvaluatorTests.swift
+++ CCNEvaluatorTests/CCNEvaluatorTests.swift
@@ -12,23 +12,87 @@ import XCTest
 class CCNEvaluatorTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
+    
+}
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+// MARK: Credit Card Number Validation
+extension CCNEvaluatorTests {
+    
+    func testIsCreditCardNumberValid() {
+        let validCardNumbers = [
+            "4929804463622139", "6762765696545485", "6210948000000029",
+            "373867484422390", "5019844570388446", "5129187892858887",
+            "6011574229193527", "348570250878868", "5128888281063960",
+            " 4716 0149 2948 1859 ", "55 233  355  605    502 4 3    "
+        ]
+        let invalidCardNumbers = [
+            "4929804463622138", "5212132012291762", "8888888888888",
+            "373867484422398", "6762765696545484", "6210948000000027",
+            "0", "1234687", "0989098909890989", "6666666666666666",
+            "ab1234??989898", "a 2 3 6789 b", "4129939187355598"
+        ]
+        
+        validCardNumbers.forEach {
+            let isValid = CCNEvaluator.isCreditCardNumberValid(creditCardNumber: $0)
+            XCTAssertEqual(isValid, true, "\($0) should be validated as a correct one.")
+        }
+        
+        invalidCardNumbers.forEach {
+            let isValid = CCNEvaluator.isCreditCardNumberValid(creditCardNumber: $0)
+            XCTAssertEqual(isValid, false, "\($0) shouldn't be validated as a correct one.")
         }
     }
-
+    
+    func testValidateOnlyNumbers() {
+        let notANumbersStrings = ["str4929804463622139",
+                                  "a b c d 87 t",
+                                  "1A2B3C4D 1234 5678 ",
+                                  "4949????49494949"]
+        notANumbersStrings.forEach {
+            let isValid = CCNEvaluator.validateOnlyNumbers(creditCardNumber: $0)
+            XCTAssertEqual(isValid, false, "\($0) with not a numbers shouldn't be validated as a correct one.")
+        }
+    }
+    
+    func testValidateNoLeadingZero() {
+        let leadingZeroString = "0"
+        let isValid = CCNEvaluator.validateNoLeadingZero(creditCardNumber: leadingZeroString)
+        XCTAssertEqual(isValid, false, "\(leadingZeroString) with leading zero shouldn't be validated as a correct one.")
+    }
+    
+    func testValidateLength() {
+        let wrongLengthStrings = ["49298044636", "49298044636221391234"]
+        wrongLengthStrings.forEach {
+            let isValid = CCNEvaluator.validateLength(creditCardNumber: $0)
+            XCTAssertEqual(isValid, false, "\($0) is too short or long and shouldn't be validated as a correct one.")
+        }
+    }
+    
+    func testValidateWithLuhnCheck() {
+        let validCardNumbers = [
+            "4929804463622139", "6762765696545485", "6210948000000029",
+            "373867484422390", "5019844570388446", "5129187892858887"
+        ]
+        let invalidCardNumbers = [
+            "4929804463622138", "5212132012291762", "8888888888888",
+            "373867484422398", "6762765696545484", "6210948000000027"
+        ]
+        
+        validCardNumbers.forEach {
+            let isValid = CCNEvaluator.validateWithLuhnCheck(creditCardNumber: $0)
+            XCTAssertEqual(isValid, true, "\($0) should be validated as a correct one.")
+        }
+        
+        invalidCardNumbers.forEach {
+            let isValid = CCNEvaluator.validateWithLuhnCheck(creditCardNumber: $0)
+            XCTAssertEqual(isValid, false, "\($0) shouldn't be validated as a correct one.")
+        }
+    }
+    
 }
